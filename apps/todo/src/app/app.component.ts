f58cbb55ae248d77e5e7049565769f9ea55df5a3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-05-02 11:56:50 +0200

    add apps

---
+++ apps/todo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-app-platform-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'todo';
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/ng-app-platform'],
+  projects: [
+    '<rootDir>/apps/ng-app-platform',
+    '<rootDir>/apps/vocabulary-trainer',
+    '<rootDir>/apps/memory',
+    '<rootDir>/apps/todo',
+    '<rootDir>/apps/weather',
+  ],
 };
